f0a7e55af411967729dcf4d1a57f7ae8cbe7b703
Atharva Sugandhi <atharva.sugandhi@gmail.com>
2023-06-15 17:21:01 +0530

    PROJECT

---
+++ Projects/My Calculator/index.js
@@ -0,0 +1,28 @@
+let string = "";
+let buttons = document.querySelectorAll(".button").length;
+
+for (let i = 0; i < buttons; i++) {
+    document
+        .querySelectorAll(".button")
+        [i].addEventListener("click", (button) => {
+            let element = button.target.innerHTML;
+
+            if (element == "C") {
+                string = "";
+            }
+            if (element == "M+") {
+                string = string * 1;
+            }
+            if (element == "+/-") {
+                string = string * -1;
+            }
+
+            if (element == "=") {
+                string = eval(string);
+            } else if (element != "C" && element != "M+" && element != "+/-") {
+                console.log(button.target);
+                string = string + element;
+            }
+            document.querySelector(".input-box").value = string;
+        });
+}
